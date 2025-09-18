00fd8b9cae9b31e39049ee40af99a0a30eb78bd2
MarkAnthonyRoxas <roxasmark20@gmail.com>
2025-09-18 19:52:39 +0800

    Program Finish

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(185, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(542, 55);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "-Before starting thread-";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 61);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(888, 505);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "BasicThread";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BasingThreading;
+using System.Threading;
+    
+
 
 namespace WindowsFormsApp2
 {
-    public partial class Form1 : Form
+
+
+public partial class Form1 : Form
+
+{
+        private Thread ThreadA, ThreadB;
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+    }
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+            Console.WriteLine("Before starting thread");
+
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+
+            ThreadA.Name = "Thread A";
+            ThreadB.Name = "Thread B";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+
+            Console.WriteLine("-End Of thread-");
+            label2.Text = "Threading Complete";
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BasicThreading_Load(object sender, EventArgs e)
+    {
+        
+
+
+          
+
+
+
+
+
+
+
+        }
+
     }
 }
+
